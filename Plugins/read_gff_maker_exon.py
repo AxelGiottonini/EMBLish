c0f313761cd6ca2e69ec9a5ed41052c1d44e3a73
AxelGiottonini <75320942+AxelGiottonini@users.noreply.github.com>
2021-05-14 09:00:02 +0200

    read_gff_maker_exon set up

--- Plugins/read_gff_maker_exon.py
+++ Plugins/read_gff_maker_exon.py
@@ -1,1 +1,28 @@
 #read_gff_maker_exon.py
+
+import pandas as pd
+import re
+
+from Bio.SeqFeature import SeqFeature, FeatureLocation, CompoundLocation
+
+
+class Plugin:
+
+    def process(self, handle, metadata, calls:list=[], target=None):
+        exons = handle.loc[(target, slice(None), "exon"),:].reset_index()
+
+        for index, exon in exons.iterrows():
+            _features_subset_ = [
+                SeqFeature(
+                    FeatureLocation(int(exon["start"]), int(exon["stop"]), (1,-1)[exon["strand"] == "-"]),
+                    type="exon",
+                    qualifiers={}
+                )]
+
+            #calls
+            receiver = []
+            for call, *args in calls:
+                receiver.extend(call.process(*args, target=(target)))
+
+             
+            yield _features_subset_
