ec0bbf41ade13e22067879db65fb9fa0cd1e3501
AxelGiottonini <75320942+AxelGiottonini@users.noreply.github.com>
2021-05-20 09:27:32 +0200

    renamed as to_handle_fasta

--- Plugins/fasta2handle.py
+++
@@ -1,9 +0,0 @@
-#fasta2handle.py
-
-from Bio import SeqIO
-
-class Plugin:
-
-    def process(self, file_path):
-        with open(file_path) as handle:
-            return list(SeqIO.parse(handle, "fasta"))
