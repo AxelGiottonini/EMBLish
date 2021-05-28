fb7e4b80b71c3b615f53ecf284e3f10eb36647b3
root <root@DESKTOP-TTGRO7N.localdomain>
2021-05-28 10:41:17 +0200

    updating structure so that each exon may be modified with callback

--- Plugins/read_gff_maker_exon.py
+++ Plugins/read_gff_maker_exon.py
@@ -20,22 +20,21 @@ class Plugin(__ReadGFFMaker__):
     
     """
     """
-    def multi_feature_initialize(self, pre_multi_feature, metadata):
-        for _, element in pre_multi_feature:
-            yield self.feature_initialize(element, metadata)
-
-
-    """
-    """
     def process(self, app, caller_mode, key_handle, calls:list=[], target=None):
         try:
-            pre_multi_feature = app.handles[key_handle].loc[(target, slice(None), "exon"),:].iterrows()
-            feature = self.multi_feature_initialize(pre_multi_feature, app.metadata)
+            multi_pre_feature = app.handles[key_handle].loc[(target, slice(None), "exon"),:].iterrows()
         except KeyError:
-            return None
-            
-        receiver = self.callbacks(
-            app,
-            calls,
-            target)
-        return self.merge(feature, receiver)
+            multi_pre_feature = []
+
+        for _, pre_feature in multi_pre_feature:
+            feature = self.feature_initialize(pre_feature, app.metadata)
+
+            try:
+                receiver = self.callbacks(
+                        app,
+                        calls,
+                        target)
+            except KeyError:
+                receiver = []
+
+            yield self.merge(feature, receiver)
